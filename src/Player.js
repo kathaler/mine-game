49ecabf6140112c483654f00bcdbca08865e312c
Karl Thaler <karlthaler5@gmail.com>
2024-09-02 22:15:28 -0700

    Add camera and refactor user controls

--- src/Player.js
+++ src/Player.js
@@ -1,89 +1,93 @@
 class Player {
-   constructor(GRID_SIZE, SPEED) {
-     this.GRID_SIZE = GRID_SIZE;
-     this.SPEED = SPEED;
- 
-     this.position = { x: 0, y: 0 };
-     this.targetPosition = { x: 0, y: 0 };
-     this.moveDirection = { x: 0, y: 0 };
-     this.moving = false;
-   }
- 
-   update(p) {
-     if (this.moving) {
-       const distanceX = this.targetPosition.x - this.position.x;
-       const distanceY = this.targetPosition.y - this.position.y;
- 
-       const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
- 
-       if (distance < this.SPEED) {
-         this.position.x = this.targetPosition.x;
-         this.position.y = this.targetPosition.y;
- 
-         if (this.moveDirection.x !== 0 || this.moveDirection.y !== 0) {
-           this.targetPosition.x += this.moveDirection.x * this.GRID_SIZE;
-           this.targetPosition.y += this.moveDirection.y * this.GRID_SIZE;
-         } else {
-           this.moving = false;
-         }
-       } else {
-         this.position.x += (distanceX / distance) * this.SPEED;
-         this.position.y += (distanceY / distance) * this.SPEED;
-       }
-     }
-   }
- 
-   draw(p) {
-     p.fill(255, 204, 0);
-     p.square(this.position.x, this.position.y, this.GRID_SIZE);
-   }
- 
-   handleKeyPressed(p) {
-     if (!this.moving) {
-       if (p.key === 'a') {
-         this.moveDirection.x = -1;
-         this.moveDirection.y = 0;
-       } else if (p.key === 'd') {
-         this.moveDirection.x = 1;
-         this.moveDirection.y = 0;
-       }
- 
-       if (p.key === 'w') {
-         this.moveDirection.y = -1;
-         this.moveDirection.x = 0;
-       } else if (p.key === 's') {
-         this.moveDirection.y = 1;
-         this.moveDirection.x = 0;
-       }
- 
-       this.targetPosition.x = this.position.x + this.moveDirection.x * this.GRID_SIZE;
-       this.targetPosition.y = this.position.y + this.moveDirection.y * this.GRID_SIZE;
- 
-       this.moving = true;
-     } else {
-       if (p.key === 'a' && this.moveDirection.y !== 0) {
-         this.moveDirection.x = -1;
-       } else if (p.key === 'd' && this.moveDirection.y !== 0) {
-         this.moveDirection.x = 1;
-       }
- 
-       if (p.key === 'w' && this.moveDirection.x !== 0) {
-         this.moveDirection.y = -1;
-       } else if (p.key === 's' && this.moveDirection.x !== 0) {
-         this.moveDirection.y = 1;
-       }
-     }
-   }
- 
-   handleKeyReleased(p) {
-     if (p.key === 'a' || p.key === 'd') {
-       this.moveDirection.x = 0;
-     }
-     if (p.key === 'w' || p.key === 's') {
-       this.moveDirection.y = 0;
-     }
-   }
- }
- 
- export default Player;
- 
+  constructor(GRID_SIZE, SPEED) {
+    this.GRID_SIZE = GRID_SIZE;
+    this.SPEED = SPEED;
+
+    this.position = { x: 0, y: 0 };
+    this.targetPosition = { x: 0, y: 0 };
+    this.moveDirection = { x: 0, y: 0 };
+    this.moving = false;
+    this.keysReleased = false;
+    this.lastKeys = [];
+  }
+
+  update(p) {
+    if (!this.keysReleased) {
+      this.position.x += this.moveDirection.x * this.SPEED;
+      this.position.y += this.moveDirection.y * this.SPEED;
+    } else {
+      const distanceX = this.targetPosition.x - this.position.x;
+      const distanceY = this.targetPosition.y - this.position.y;
+
+      const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+      if (distance < this.SPEED) {
+        this.position.x = this.targetPosition.x;
+        this.position.y = this.targetPosition.y;
+        this.moving = false;
+      } else {
+        this.position.x += (distanceX / distance) * this.SPEED;
+        this.position.y += (distanceY / distance) * this.SPEED;
+      }
+    }
+  }
+
+  draw(p) {
+    p.fill(255, 204, 0);
+    p.square(this.position.x, this.position.y, this.GRID_SIZE);
+  }
+
+  handleKeyPressed(p) {
+    this.keysReleased = false;
+
+    if (p.key === "a") {
+      this.moveDirection.x -= 1;
+    }
+    if (p.key === "d") {
+      this.moveDirection.x += 1;
+    }
+    if (p.key === "w") {
+      this.moveDirection.y -= 1;
+    }
+    if (p.key === "s") {
+      this.moveDirection.y += 1;
+    }
+  }
+
+  handleKeyReleased(p) {
+    if (p.key === "a" || p.key === "d") {
+      this.moveDirection.x = 0;
+    }
+    if (p.key === "w" || p.key === "s") {
+      this.moveDirection.y = 0;
+    }
+
+    // Snap to the closest grid unit
+    if (this.moveDirection.x === 0 && this.moveDirection.y === 0) {
+      this.keysReleased = true;
+      let closestGridX =
+        Math.round(this.position.x / this.GRID_SIZE) * this.GRID_SIZE;
+      let closestGridY =
+        Math.round(this.position.y / this.GRID_SIZE) * this.GRID_SIZE;
+
+
+      if (p.key === "a") {
+        closestGridX -= this.GRID_SIZE;
+      }
+      if (p.key === "d") {
+        closestGridX += this.GRID_SIZE;
+      }
+      if (p.key === "w") {
+        closestGridY -= this.GRID_SIZE;
+      }
+      if (p.key === "s") {
+        closestGridY += this.GRID_SIZE;
+      }
+
+      this.targetPosition.x = closestGridX;
+      this.targetPosition.y = closestGridY;
+    }
+  }
+}
+
+export default Player;
