add6e1bb29e725906c413e3f1e1135f8eadb2ddf
Karl Thaler <karlthaler5@gmail.com>
2024-09-06 09:04:24 -0700

    Use coordinate class for tracking tile coordinates

--- src/Grid.js
+++ src/Grid.js
@@ -6,10 +6,10 @@ class Grid {
     this.GRID_SIZE = size;
     this.PLAYER = player;
 
-   this.width = width;
-   this.height = height;
+    this.width = width;
+    this.height = height;
 
-   this.tiles = new Map();
+    this.tiles = new Map();
   }
 
   initializeGrid() {
@@ -19,41 +19,44 @@ class Grid {
     const startY = this.PLAYER.position.y - 2;
     const endY = this.PLAYER.position.y + 2;
 
-    for (let x = -this.width/2; x <= this.width/2; x += 1) {
-      for (let y = -this.height/2; y <= this.height/2; y += 1) {
-        if (x >= startX && x <= endX && y >= startY && y <= endY) {
-          continue;
-        }
+    for (let x = -this.width / 2; x <= this.width / 2; x += 1) {
+      for (let y = -this.height / 2; y <= this.height / 2; y += 1) {
         const tile = new Tile(x, y, this.GRID_SIZE);
+        if (x >= startX && x <= endX && y >= startY && y <= endY) {
+          tile.mine();
+        }
         this.tiles.set(`${x},${y}`, tile);
-
       }
     }
   }
 
-  drawGrid(p, viewLeft, viewRight, viewTop, viewBottom) {   
-   p.push();
-   p.stroke(75);
+  drawGrid(p, viewLeft, viewRight, viewTop, viewBottom) {
+    p.push();
+    p.stroke(75);
 
-   for (let x = viewLeft; x <= viewRight; x++) {
+    for (let x = viewLeft; x <= viewRight; x++) {
       for (let y = viewTop; y <= viewBottom; y++) {
-         const tile = this.tiles.get(`${x},${y}`);
-         if (tile && !tile.isMined) {
-            tile.draw(p);
-         }
+        const tile = this.tiles.get(`${x},${y}`);
+        if (tile && !tile.isMined) {
+          tile.draw(p);
+        }
       }
-   }
- 
-   p.pop();
- }
+    }
+
+    p.pop();
+  }
 
   updateGrid(p) {
-    const { x: playerX, y: playerY } = convertToScreenPosition(this.PLAYER.position.x, this.PLAYER.position.y, this.GRID_SIZE);
+    const { x: playerX, y: playerY } = convertToScreenPosition(
+      this.PLAYER.position.x,
+      this.PLAYER.position.y,
+      this.GRID_SIZE
+    );
 
     const viewLeft = Math.floor(playerX - this.width / 2);
-      const viewRight = Math.floor(playerX + this.width / 2);
-      const viewTop = Math.floor(playerY - this.height / 2);
-      const viewBottom = Math.floor(playerY + this.height / 2);
+    const viewRight = Math.floor(playerX + this.width / 2);
+    const viewTop = Math.floor(playerY - this.height / 2);
+    const viewBottom = Math.floor(playerY + this.height / 2);
 
     this.generateNewTiles(viewLeft, viewRight, viewTop, viewBottom);
     this.removeTouchedTiles(playerX, playerY);
@@ -61,66 +64,71 @@ class Grid {
   }
 
   removeTouchedTiles(playerX, playerY) {
-   const playerLeft = playerX;
-   const playerRight = playerX + 1;
-   const playerTop = playerY;
-   const playerBottom = playerY + 1;
+    const playerLeft = playerX;
+    const playerRight = playerX + 1;
+    const playerTop = playerY;
+    const playerBottom = playerY + 1;
 
-   console.log("player", playerLeft, playerRight, playerTop, playerBottom);
- 
-   for (const [key, tile] of this.tiles.entries()) {
-     const tileLeft = tile.position.x / this.GRID_SIZE;
-     const tileRight = (tile.position.x + this.GRID_SIZE)/this.GRID_SIZE;
-     const tileTop = tile.position.y / this.GRID_SIZE;
-     const tileBottom = (tile.position.y + this.GRID_SIZE)/this.GRID_SIZE;
- 
-     const overlapX = playerRight > tileLeft && playerLeft < tileRight;
-     const overlapY = playerBottom > tileTop && playerTop < tileBottom;
- 
-     if (overlapX && overlapY && !tile.isMined) {
-      console.log("Touched tile", tile.position.x, tile.position.y);
-       tile.mine(); 
-     }
-   }
- }
- 
+    for (const [key, tile] of this.tiles.entries()) {
+      const gridPosition = tile.coords.gridPosition();
 
-generateNewTiles(viewLeft, viewRight, viewTop, viewBottom) {
-   const extra = this.GRID_SIZE * 2;
-   let tileCount = 0;
-   const maxTilesPerFrame = 10;
- 
-   for (let x = viewLeft; x <= viewRight && tileCount < maxTilesPerFrame; x += 1) {
-     if (!this.doesTileExist(x, viewTop)) {
-       const tile = new Tile(x, viewTop, this.GRID_SIZE);
-       this.tiles.set(`${x},${viewTop}`, tile);
-       tileCount++;
-     }
-     if (!this.doesTileExist(x, viewBottom) && tileCount < maxTilesPerFrame) {
-       const tile = new Tile(x, viewBottom, this.GRID_SIZE);
-       this.tiles.set(`${x},${viewBottom}`, tile);
-       tileCount++;
-     }
-   }
- 
-   for (let y = viewTop; y <= viewBottom && tileCount < maxTilesPerFrame; y += 1) {
-     if (!this.doesTileExist(viewLeft, y)) {
-       const tile = new Tile(viewLeft, y, this.GRID_SIZE);
-       this.tiles.set(`${viewLeft},${y}`, tile);
-       tileCount++;
-     }
-     if (!this.doesTileExist(viewRight, y) && tileCount < maxTilesPerFrame) {
-       const tile = new Tile(viewRight, y, this.GRID_SIZE);
-       this.tiles.set(`${viewRight},${y}`, tile);
-       tileCount++;
-     }
-   }
- }
- 
+      const tileLeft = gridPosition.x;
+      const tileRight = gridPosition.x + 1;
+      const tileTop = gridPosition.y;
+      const tileBottom = gridPosition.y + 1;
 
- doesTileExist(x, y) {
-   return this.tiles.has(`${x},${y}`);
- }
+      const overlapX = playerRight > tileLeft && playerLeft < tileRight;
+      const overlapY = playerBottom > tileTop && playerTop < tileBottom;
+
+      if (overlapX && overlapY && !tile.isMined) {
+        tile.mine();
+      }
+    }
+  }
+
+  generateNewTiles(viewLeft, viewRight, viewTop, viewBottom) {
+    const extra = this.GRID_SIZE * 2;
+    let tileCount = 0;
+    const maxTilesPerFrame = 10;
+
+    for (
+      let x = viewLeft;
+      x <= viewRight && tileCount < maxTilesPerFrame;
+      x += 1
+    ) {
+      if (!this.doesTileExist(x, viewTop)) {
+        const tile = new Tile(x, viewTop, this.GRID_SIZE);
+        this.tiles.set(`${x},${viewTop}`, tile);
+        tileCount++;
+      }
+      if (!this.doesTileExist(x, viewBottom) && tileCount < maxTilesPerFrame) {
+        const tile = new Tile(x, viewBottom, this.GRID_SIZE);
+        this.tiles.set(`${x},${viewBottom}`, tile);
+        tileCount++;
+      }
+    }
+
+    for (
+      let y = viewTop;
+      y <= viewBottom && tileCount < maxTilesPerFrame;
+      y += 1
+    ) {
+      if (!this.doesTileExist(viewLeft, y)) {
+        const tile = new Tile(viewLeft, y, this.GRID_SIZE);
+        this.tiles.set(`${viewLeft},${y}`, tile);
+        tileCount++;
+      }
+      if (!this.doesTileExist(viewRight, y) && tileCount < maxTilesPerFrame) {
+        const tile = new Tile(viewRight, y, this.GRID_SIZE);
+        this.tiles.set(`${viewRight},${y}`, tile);
+        tileCount++;
+      }
+    }
+  }
+
+  doesTileExist(x, y) {
+    return this.tiles.has(`${x},${y}`);
+  }
 }
 
 export default Grid;
--- src/Tile.js
+++ src/Tile.js
@@ -1,24 +1,28 @@
 import { convertToGridPosition } from './utils';
+import Coordinates from './Coordinates';
 
 class Tile {
    constructor(x, y, size) {
       this.size = size;
       this.isMined = false;
-      this.position = convertToGridPosition(x, y, size);
+      this.coords = new Coordinates(x, y, size);
    }
 
    draw(p) {
+      const screenPosition = this.coords.screenPosition();
+
       p.fill(211, 211, 211);
-      p.square(this.position.x, this.position.y, this.size);
+      p.square(screenPosition.x, screenPosition.y, this.size);
 
       p.textSize(12);
       p.fill(0);
 
-      let textX = this.position.x + this.size / 2;
-      let textY = this.position.y + this.size / 2 + p.textSize() / 3;
+      const gridPosition = this.coords.gridPosition();
+      let textX = screenPosition.x + this.size / 2;
+      let textY = screenPosition.y + this.size / 2 + p.textSize() / 3;
 
       p.textAlign(p.CENTER, p.CENTER);
-      p.text(this.position.x / this.size + "," + this.position.y / this.size, textX, textY);
+      p.text(gridPosition.x + "," + gridPosition.y, textX, textY);
    }
 
    mine() {
