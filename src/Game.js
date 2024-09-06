6f35a93b8ee55474b4b8d326669c6bd15173ba5c
Karl Thaler <karlthaler5@gmail.com>
2024-09-06 08:06:36 -0700

    Use camera to follow player

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,7 @@
 // Game.js
 import Player from "./Player";
 import Grid from "./Grid";
+import Camera from "./Camera";
 
 class Game {
   constructor(GAME_WIDTH, GAME_HEIGHT, GRID_SIZE, SPEED) {
@@ -10,18 +11,20 @@ class Game {
     this.SPEED = SPEED;
 
     this.player = new Player(GRID_SIZE, SPEED);
-    this.grid = new Grid(GRID_SIZE, this.player);
+    this.grid = new Grid(GRID_SIZE, this.player, this.GAME_WIDTH, this.GAME_HEIGHT);
+    this.camera = new Camera();
 
-    // Camera position and target position
-    this.cameraPosition = { x: 0, y: 0 };
-    this.cameraTarget = { x: 0, y: 0 };
-    this.cameraSpeed = 0.1; // Adjust this value to control lag
+    this.offSetX = (this.GAME_WIDTH * this.GRID_SIZE) / 2;
+      this.offSetY = (this.GAME_HEIGHT * this.GRID_SIZE) / 2;
   }
 
   setup(p) {
-    p.createCanvas(this.GAME_WIDTH, this.GAME_HEIGHT);
+    p.createCanvas(document.documentElement.clientWidth, document.documentElement.clientHeight);
     p.background(135, 206, 235);
-    this.grid.generateGrid(this.GAME_WIDTH, this.GAME_HEIGHT);
+    this.grid.initializeGrid();
+
+    this.camera.position = { x: this.player.position.x - this.offSetX, y: this.player.position.y - this.offSetY };
+   this.camera.targetPosition = { x: this.player.position.x - this.offSetX, y: this.player.position.y - this.offSetY };
   }
 
   windowResized(p) {
@@ -34,24 +37,19 @@ class Game {
     p.background(135, 206, 235);
 
     // Calculate the target camera position (centered on the player)
-    this.cameraTarget.x =
-      this.player.position.x - this.GAME_WIDTH / 2 + this.GRID_SIZE / 2;
-    this.cameraTarget.y =
-      this.player.position.y - this.GAME_HEIGHT / 2 + this.GRID_SIZE / 2;
+    this.camera.targetPosition.x =
+      this.player.position.x - this.offSetX;
+    this.camera.targetPosition.y =
+      this.player.position.y - this.offSetY;
 
-    // Smoothly move the camera position towards the target position
-    this.cameraPosition.x +=
-      (this.cameraTarget.x - this.cameraPosition.x) * this.cameraSpeed;
-    this.cameraPosition.y +=
-      (this.cameraTarget.y - this.cameraPosition.y) * this.cameraSpeed;
-
-    // Apply camera translation
-    p.translate(-this.cameraPosition.x, -this.cameraPosition.y);
+    this.camera.position.x +=
+    (this.camera.targetPosition.x - this.camera.position.x) * this.camera.speed;
+  this.camera.position.y +=
+    (this.camera.targetPosition.y - this.camera.position.y) * this.camera.speed;
+  
+    p.translate(-this.camera.position.x, -this.camera.position.y);
 
     this.grid.updateGrid(p);
-
-    // Draw the grid and player with the camera translation applied
-    this.grid.drawGrid(p, this.GAME_WIDTH, this.GAME_HEIGHT);
     this.player.update(p);
     this.player.draw(p);
   }
